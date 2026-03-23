a4c2a51ad426da623b2cb036dca02da1033b9d86
Jay <jayyadav1309@gmail.com>
2026-03-24 00:47:23 +0530

    First commit

---
+++ src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
@@ -0,0 +1,51 @@
+package com.springrest.springrest.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.dao.CourseDao;
+import com.springrest.springrest.entity.Course;
+
+
+@Service
+public class CourseServiceImpl implements CourseService  {
+	
+	@Autowired
+	private CourseDao courseDao; 
+	
+	@Override
+	public List<Course> getCourses() {
+		
+		return courseDao.findAll() ;
+	}
+
+    @Override
+	public Course getCourse(long courseId) {
+		
+		return courseDao.getOne(courseId);
+	}
+
+
+	@Override
+	public Course addCourse(Course course) {
+		
+		courseDao.save(course);
+		return course;
+	}
+	
+	@Override
+	public Course updateCourse(Course course)
+	{
+		courseDao.save(course);
+		return course;
+	}
+	
+	@Override
+	public void deleteCourse(long parseLong)
+	{
+		Course entity = courseDao.getOne(parseLong);
+				courseDao.delete(entity);
+	}
+
+}
