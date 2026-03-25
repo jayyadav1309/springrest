d499b07a3a5132c780c2d2b1b276717deb5311f9
jayyadav1309 <jayyadav1309@gmail.com>
2026-03-25 15:57:36 +0530

    Update MyController.java

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.springrest.springrest.entity.Course;
 import com.springrest.springrest.service.CourseService;
 
-@CrossOrigin(origins = "http://youtube-courses-production.up.railway.app/")
+@CrossOrigin(origins = "https://youtube-courses-production.up.railway.app/")
 @RestController
 public class MyController {
 	
